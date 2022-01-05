646fc2a40c548f5b0c05fae64d30ed78df57ffb6
itsmrmonday <zackary.live8@gmail.com>
2022-01-04 21:03:56 -0500

    compiler

---
+++ cxpressCompiler/README.md
@@ -0,0 +1,15 @@
+# Compilers
+I am currently working on the first compiler and making good progress everyday,
+a python3 version of the compiler will be made to compile a cXpress written compiler
+
+# cxCompiler
+    Usages:
+        cxc <build_file_path>
+        cxc <cx file> <project type> <project name>
+
+
+# pyCompiler
+    Usages:
+        compiler.py <build_file_path>
+        csc <cx file> <project type> <project name>
+
--- cxpressCompiler/pyCompiler/compiler.py
+++ cxpressCompiler/pyCompiler/compiler.py
@@ -1,1 +1,18 @@
-import osimport sysdef args_check(args):    if len(args) < 2:        return False    def main(args):    #Get C# converted file and compile using dotnet and the system provided    if args_check(args) == False:        return 1    if __name__ == '__main__':        return_code = main(sys.argv)    sys.exit(return_code)
+import os
+import sys
+import parser
+
+def args_check(args):
+    if len(args) < 2:
+        return False
+    
+
+def main(args):
+    #Get C# converted file and compile using dotnet and the system provided
+    if args_check(args) == False:
+        return 1
+    
+if __name__ == '__main__':
+    
+    return_code = main(sys.argv)
+    sys.exit(return_code)
