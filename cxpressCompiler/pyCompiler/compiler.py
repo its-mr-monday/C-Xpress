7a573bcebc03929f79f40807315bfc4f5c371c79
itsmrmonday <zackary.live8@gmail.com>
2022-01-05 10:29:20 -0500

    switch case addition

--- cxpressCompiler/pyCompiler/compiler.py
+++ cxpressCompiler/pyCompiler/compiler.py
@@ -2,6 +2,16 @@ import os
 import sys
 import parser
 
+def shell_compiler_cmd(build_file: str):
+    try:
+        cmd = "msbuild " + build_file + " -t:Build"
+        result = os.system(cmd)
+        print(result)
+        return True
+    except Exception:
+        return False
+        
+
 def args_check(args):
     if len(args) < 2:
         return False
